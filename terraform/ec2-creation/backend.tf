6c512ba7b67b08e5f9737637f3d42cc5d8063853
Asel Nazarova <aselnazarovaa@gmail.com>
2021-05-27 20:07:31 -0500

    added

--- terraform/ec2-creation/backend.tf
+++ terraform/ec2-creation/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-file-asel"
-    key    = "tfstate/sample-module10.tfstate"
-    region = "us-east-1"
+    bucket  = "teamhulk-tfstate-backend-bucket"
+    key     = "tfstate/backend1.tfstate"
+    region  = "us-east-1"
   }
 }
